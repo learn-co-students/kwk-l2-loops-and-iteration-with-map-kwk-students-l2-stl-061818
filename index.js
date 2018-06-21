e3de4b9b6099ca620c6856a986e917d594b7c7d4
Kailin Zhang <kailin11511@gmail.com>
2018-06-21 19:10:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution in this file.
+
+// let lowerCaseStudenNames =['Adele', '']
+
+function other (student) {
+  return student.toLowerCase();
+}
+
+function lowerCaseStudentNames (list) {
+return list.map(other)
+}
+
+function splitName(name) {
+  const studentFirst = name.split(" ")[0];
+  const studentLast = name.split(" ")[1];
+  
+  return { firstName : studentFirst, lastName : studentLast};
+}
+
+function nameToAttributes(list) {
+  return list.map(splitName);
+}
+
+
