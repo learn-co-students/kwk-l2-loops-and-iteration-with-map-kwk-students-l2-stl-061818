d4a1ccc5a2cd62b8d974d3a275a7f445be729bad
Hannah Shine <hshine@visitationacademy.org>
2018-06-21 18:47:46 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solution in this file.
+// // Code your solution in this file.
+// let names=['Adele','Beyonce','Lady','Madonna','Rihanna','Taylor']
+// function lowerCasestudentNames(names){
+//   console.log(names)
+// }
+
+function lowerCaseStudentNames(students){
+  return students.map(function (name){
+    return name.toLowerCase()
+  });
+}
+console.log(lowerCaseStudentNames(['Adele', 'Beyoncé', 'Lady', 'Madonna', 'Rihanna', 'Taylor']))
+
+function nameToAttributes(names){
+  return names.map(function (celebrity){
+    let first=celebrity.split(" ")[0] //splits up string by spaces
+    let last=celebrity.split(" ")[1]
+    return { firstName:first, lastName:last}
+  })
+}
+
+function attributesToPhrase(info){
+  return info.map(function(person){
+    return `${person.name} is from ${person.hometown}`;
+  })
+}
